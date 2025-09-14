f3b446d7fa89e97af01e70f62eb81100e8d0dc87
Hidetake Iwata <int128@gmail.com>
2025-09-14 21:44:14 +0900

    Add retry middleware

---
+++ src/retry.ts
@@ -0,0 +1,38 @@
+import * as core from '@actions/core'
+import { LanguageModelMiddleware } from 'ai'
+
+export const retryMiddleware: LanguageModelMiddleware = {
+  wrapGenerate: async ({ doGenerate }) => {
+    for (let attempt = 0; ; attempt++) {
+      try {
+        return await doGenerate()
+      } catch (error: unknown) {
+        if (attempt > 3) {
+          throw error
+        }
+        if (error instanceof Error) {
+          const retryAfterSec = parseRetryAfterSec(error.message)
+          if (retryAfterSec) {
+            core.warning(`Retry attempt ${attempt} after ${retryAfterSec}s: ${error}`)
+            await new Promise((resolve) => setTimeout(resolve, retryAfterSec * 1000))
+            continue
+          }
+        }
+        throw error
+      }
+    }
+  },
+}
+
+const parseRetryAfterSec = (message: string): number | undefined => {
+  if (message.match(/"code":429|"code":500/)) {
+    const m = message.match(/"retryDelay":"(\d+)s"/)
+    if (m) {
+      const s = Number.parseInt(m[1])
+      if (Number.isSafeInteger(s) && s > 0) {
+        return s
+      }
+    }
+    return 30
+  }
+}
