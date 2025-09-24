71966e71494b6776b828b347ebd165b34c488485
Hidetake Iwata <int128@gmail.com>
2025-09-24 09:47:47 +0900

    Improve readFile and editFile (#277)

--- src/coding/readFile.ts
+++ src/coding/readFile.ts
@@ -5,38 +5,48 @@ import { z } from 'zod'
 
 export const readFileTool = createTool({
   id: 'readFile',
-  description: 'Read a file.',
+  description: `Read the lines from a file.`,
   inputSchema: z.object({
     path: z.string().describe('The path to the file in the repository.'),
+    offset: z
+      .int()
+      .min(0)
+      .describe(`The 0-based address of the first line to read.
+For example, if you want to read from the beginning of the file, set this to 0.
+If you want to read from the 101st line, set this to 100.
+`),
   }),
   outputSchema: z.object({
     lines: z
       .array(
         z.object({
           address: z
-            .number()
             .int()
             .min(0)
-            .describe('The address of the line in the file. Address 0 is the first line.'),
+            .describe('The address of the line in the file. Address 0 is the first line in the file.'),
           line: z
             .string()
             .describe('A line content at the address. This string does not include a trailing newline character.'),
         }),
       )
-      .describe('The array of lines read from the file.'),
+      .max(100)
+      .describe('The array of lines read from the file. Up to 100 lines are returned.'),
+    total: z.int().describe('The total number of lines in the file.'),
   }),
   execute: async ({ context }) => {
     const fileContent = await fs.readFile(context.path, 'utf-8')
-    const lines = fileContent.split('\n').map((line, address) => ({ address, line }))
-    core.startGroup(`🤖 Reading ${context.path} (${lines.length} lines)`)
-    for (const { address, line } of lines) {
+    const allLines = fileContent.split('\n').map((line, address) => ({ address, line }))
+    const partialLines = allLines.slice(context.offset, context.offset + 100)
+    core.startGroup(`🤖 Reading ${context.path} (offset: ${context.offset})`)
+    for (const { address, line } of partialLines) {
       core.info(`${address}: ${line}`)
     }
     core.endGroup()
-    core.summary.addHeading(`🔧 Read a file (${lines.length} lines)`, 3)
+    core.summary.addHeading(`🔧 Read a file (offset: ${context.offset})`, 3)
     core.summary.addCodeBlock(context.path)
     return {
-      lines,
+      lines: partialLines,
+      total: allLines.length,
     }
   },
 })
