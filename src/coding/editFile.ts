2a24e7bf6a241ba2a36e8b2062ef0a6a66eacb41
Hidetake Iwata <int128@gmail.com>
2025-09-24 09:00:13 +0900

    Improve editFile

--- src/coding/editFile.ts
+++ src/coding/editFile.ts
@@ -6,35 +6,36 @@ import { z } from 'zod'
 
 export const editFileTool = createTool({
   id: 'editFile',
-  description: 'Edit an existing file.',
+  description: `Manipulate the lines of an existing file.
+This tool applies the patches in order and finally writes the lines to the file.
+`,
   inputSchema: z.object({
     path: z.string().describe('The path to the file in the repository. The file must exist.'),
     patches: z
       .array(
         z.object({
           address: z
-            .number()
             .int()
             .min(0)
-            .describe(`The 0-based address of the line in the file. Address 0 is the first line.`),
-          operation: z.enum(['REPLACE', 'DELETE']).describe(`The operation for the line:
-- If REPLACE is set, replace the line content. If you want to insert a new line, replace an existing line with multiple lines separated by newline characters.
-- If DELETE is set, mark the address for deletion.
+            .describe(`The 0-based address of the line in the file.
+Address 0 is the first line.
+The addresses in the file are not changed during the patch operations.
 `),
           replacement: z
             .string()
             .optional()
-            .describe('The text to replace the line with. Required for REPLACE operation.'),
-          insertion: z
-            .string()
-            .optional()
-            .describe('The text to insert before the line. Required for INSERT_BEFORE operation.'),
+            .describe(`The new content for the address.
+For example:
+- If "FOO" is given, replace the content of the line with FOO.
+- If "FOO\\n" is given, insert a new line after FOO. The addresses of the consequent lines are not changed.
+- If "FOO\\nBAR" is given, replace the line with the 2 lines. The addresses of the consequent lines are not changed.
+- If "" is given, replace the line with an empty line.
+- If this is not set, remove the line. The addresses of the consequent lines are not changed.
+`),
         }),
       )
       .min(1)
-      .describe(`An array of patches to perform on the file.
-The patches will be applied in the order they are specified.
-`),
+      .describe(`An array of patches to manipulate the lines of the file.`),
   }),
   outputSchema: z.object({
     changes: z
@@ -42,7 +43,10 @@ The patches will be applied in the order they are specified.
         z.object({
           address: z.number().int().min(0).describe('The address of the line in the file.'),
           original: z.string().optional().describe('The original content of the line.'),
-          updated: z.string().optional().describe('The updated content of the line.'),
+          updated: z
+            .string()
+            .optional()
+            .describe('The updated content of the line. If this is not set, the line will be removed.'),
         }),
       )
       .describe('An array of changes made to the file.'),
@@ -57,19 +61,9 @@ The patches will be applied in the order they are specified.
         address >= 0 && address < lines.length,
         `address must be between 0 and ${lines.length - 1} but got ${address}`,
       )
-      switch (patch.operation) {
-        case 'REPLACE': {
-          const original = lines[address]
-          lines[address] = patch.replacement
-          changes.push({ address, original, updated: patch.replacement })
-          break
-        }
-        case 'DELETE': {
-          changes.push({ address, original: lines[address], updated: undefined })
-          lines[address] = undefined // Mark for deletion
-          break
-        }
-      }
+      const original = lines[address]
+      lines[address] = patch.replacement
+      changes.push({ address, original, updated: patch.replacement })
     }
 
     core.info(`🤖 Edited ${context.path} (${lines.length} lines)`)
@@ -79,14 +73,15 @@ The patches will be applied in the order they are specified.
     core.summary.addHeading(`🔧 Edit a file (${lines.length} lines)`, 3)
     core.summary.addCodeBlock(context.path)
     for (const change of changes) {
-      core.info(`- ${change.address}: ${change.original ?? ''}`)
-      core.info(`+ ${change.address}: ${change.updated ?? ''}`)
-      core.summary.addCodeBlock(
-        `\
-- ${change.address}: ${change.original ?? ''}
-+ ${change.address}: ${change.updated ?? ''}`,
-        'diff',
-      )
+      const diff = []
+      if (change.original) {
+        diff.push(`- ${change.address}: ${change.original}`)
+      }
+      if (change.updated) {
+        diff.push(`+ ${change.address}: ${change.updated}`)
+      }
+      core.info(diff.join('\n'))
+      core.summary.addCodeBlock(diff.join('\n'), 'diff')
     }
 
     const newContent = lines.filter((line) => line !== undefined).join('\n')
