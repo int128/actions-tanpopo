441e033fa596364f9b88510c694dc1dbf9468b0e
Hidetake Iwata <int128@gmail.com>
2025-09-28 16:53:16 +0900

    Fix

--- tasks/setup-node-v5/precondition.sh
+++ tasks/setup-node-v5/precondition.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eux -o pipefail
 
+if [[ ! -d .github/workflows ]]; then
+  exit 99 # Skip the task
+fi
+
 if ! grep 'uses: actions/setup-node' .github/workflows/*; then
   exit 99 # Skip the task
 fi
