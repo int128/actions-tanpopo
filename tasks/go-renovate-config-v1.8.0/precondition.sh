0d82eb5c290146eb445bd18aaee2229c56cc55b0
Hidetake Iwata <int128@gmail.com>
2025-10-25 19:18:59 +0900

    Fix

--- tasks/go-renovate-config-v1.8.0/precondition.sh
+++ tasks/go-renovate-config-v1.8.0/precondition.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -eux -o pipefail
 
 if [ ! -f .github/renovate.json* ]; then
   exit 99 # skip the task
 fi
 
-perl -i -pe 's/github>int128\/go-renovate-config#v1.7.2/github>int128/go-renovate-config#v1.8.0/g' .github/renovate.json*
+perl -i -pe 's/github>int128\/go-renovate-config#v1.7.2/github>int128\/go-renovate-config#v1.8.0/g' .github/renovate.json*
 
 perl -i -pe 's/ +"github>int128\/go-renovate-config:github-actions#v1.7.2",?\n//g' .github/renovate.json*
 
