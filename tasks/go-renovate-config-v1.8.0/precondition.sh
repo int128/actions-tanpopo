cbe7908bd68e480e55c35f004ca28060207b3f6e
Hidetake Iwata <int128@gmail.com>
2025-10-25 19:29:41 +0900

    Fix

--- tasks/go-renovate-config-v1.8.0/precondition.sh
+++ tasks/go-renovate-config-v1.8.0/precondition.sh
@@ -11,6 +11,8 @@ perl -i -pe 's/ +"github>int128\/go-renovate-config:github-actions#v1.7.2",?\n//
 
 perl -i -pe 's/github>int128\/go-renovate-config:kustomization-github-releases#v1.7.2/github>int128\/go-renovate-config:github-releases#v1.8.0(**\/kustomization.yaml)/g' .github/renovate.json*
 
+perl -i -pe 's/github>int128\/go-renovate-config:doc-github-releases#v1.7.2/github>int128\/go-renovate-config:github-releases#v1.8.0(README.md)/g' .github/renovate.json*
+
 if [ -z "$(git status --porcelain)" ]; then
   exit 99 # skip the task
 fi
