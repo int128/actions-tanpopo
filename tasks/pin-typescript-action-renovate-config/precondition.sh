246bac53c549b6238d6f8d2bd6e9f1f3e22f8786
Hidetake Iwata <int128@gmail.com>
2025-12-06 14:52:00 +0900

    Fix

--- tasks/pin-typescript-action-renovate-config/precondition.sh
+++ tasks/pin-typescript-action-renovate-config/precondition.sh
@@ -2,4 +2,8 @@
 
 perl -i -pe 's/"github>int128\/typescript-action-renovate-config"/"github>int128\/typescript-action-renovate-config#v1.9.0"/g' .github/renovate.json*
 
+if [ -z "$(git status --porcelain)" ]; then
+  exit 99 # skip the task
+fi
+
 exit 0
