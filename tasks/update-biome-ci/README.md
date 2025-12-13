2ead8c6df4e80ebcb72e4ebaa6de1262b31a7e08
Hidetake Iwata <int128@gmail.com>
2025-12-13 14:07:52 +0900

    Update biome.json in GitHub Actions (#362)
    * Update biome.json in GitHub Actions
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Add
    
    * Fix
    
    * All

---
+++ tasks/update-biome-ci/README.md
@@ -0,0 +1,10 @@
+# Update biome.json in GitHub Actions
+
+## Goal
+
+When `@biomejs/biome` is updated, we want to automatically update the version in `biome.json`.
+
+## Step
+
+The workspace is already updated by [precondition.sh](./precondition.sh).
+Your task is generating the description of the current change.
---
+++ tasks/update-biome-ci/precondition.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eux -o pipefail
+
+if [[ ! -d .github/workflows ]]; then
+  exit 99 # Skip the task
+fi
+
+if ! grep '@biomejs/biome' package.json; then
+  exit 99 # Skip the task
+fi
+
+if grep 'biome migrate --write' package.json; then
+  exit 99 # Skip the task
+fi
+
+jq '.scripts.check = "biome migrate --write && biome check --fix"' package.json > package.tmp.json
+mv package.tmp.json package.json
+
+perl -i -pne 's/run: pnpm biome check --fix/run: pnpm run check/' .github/workflows/*.yaml
+
+pnpm install
+pnpm run check
+
+if git diff --quiet; then
+  exit 99 # Skip the task
+fi
+
+exit 0
