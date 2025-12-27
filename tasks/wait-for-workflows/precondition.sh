75ced3eb662018bc935c6156a8b8a90ade46de2f
Hidetake Iwata <int128@gmail.com>
2025-12-27 21:32:02 +0900

    Fix

--- tasks/wait-for-workflows/precondition.sh
+++ tasks/wait-for-workflows/precondition.sh
@@ -11,6 +11,7 @@ fi
 
 cp /home/runner/work/actions-tanpopo/actions-tanpopo/.github/workflows/wait-for-workflows.yaml .github/workflows/wait-for-workflows.yaml
 
+git add .
 if git diff --quiet; then
   exit 99 # Skip the task
 fi
