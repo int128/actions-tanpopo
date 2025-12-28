87b590e7f610f333d2a277b9cf538ca1bcc78bd5
Hidetake Iwata <int128@gmail.com>
2025-12-28 15:53:29 +0900

    add condition

--- tasks/migrate-renovate-json5/precondition.sh
+++ tasks/migrate-renovate-json5/precondition.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+set -eux -o pipefail
+
+if [ -f ".github/renovate.json" ]; then
+  echo "renovate.json already exists. Skipping migration."
+  exit 99
+fi
+
+if [ ! -f ".github/renovate.json5" ]; then
+  echo "renovate.json5 does not exist. Nothing to migrate."
+  exit 99
+fi
+
 exit 0
