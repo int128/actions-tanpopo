cd8efc882562d5ae93d8d4e6fa46d9e5c972e143
Hidetake Iwata <int128@gmail.com>
2025-12-30 16:48:37 +0900

    Extract Task type

---
+++ src/task.ts
@@ -0,0 +1,32 @@
+import * as fs from 'node:fs/promises'
+import * as path from 'node:path'
+import type { WebhookEvent } from '@octokit/webhooks-types'
+import type { Context } from './github.ts'
+
+export type Task = {
+  name: string
+  repositories: string[]
+  instruction: string
+  preconditionScriptPath: string
+}
+
+export const parseTask = async (taskName: string, context: Context<WebhookEvent>) => {
+  const taskDir = path.join(context.workspace, 'tasks', taskName)
+  const repositories = parseRepositoriesFile(await fs.readFile(path.join(taskDir, 'repositories'), 'utf-8'))
+  const instruction = await fs.readFile(path.join(taskDir, 'README.md'), 'utf-8')
+  return {
+    name: taskName,
+    repositories,
+    instruction,
+    preconditionScriptPath: path.join(taskDir, 'precondition.sh'),
+  }
+}
+
+const parseRepositoriesFile = (s: string): string[] => [
+  ...new Set(
+    s
+      .split('\n')
+      .map((line) => line.replace(/#.*$/, '').trim())
+      .filter((line) => line !== ''),
+  ),
+]
