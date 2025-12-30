53e2e25e17cb555855286f6a2673719e8ef80f4f
Hidetake Iwata <int128@gmail.com>
2025-12-30 20:25:39 +0900

    Skip coding agent if precondition.sh returned 109

--- src/run.ts
+++ src/run.ts
@@ -3,10 +3,9 @@ import * as fs from 'node:fs/promises'
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import type { Octokit } from '@octokit/action'
-import { runCodingAgent } from './coding/agent.ts'
 import * as git from './git.ts'
 import type { Context } from './github.ts'
-import { parseTask, type Task } from './task.ts'
+import { performTask, parseTask, type Task } from './task.ts'
 
 type Inputs = {
   tasks: string[]
@@ -72,36 +71,33 @@ const processRepository = async (repository: string, task: Task, octokit: Octoki
   }
 
   core.summary.addHeading(`Repository ${repository}`, 2)
-  const response = await runCodingAgent({
-    taskInstruction: task.instruction,
-    githubContext: context,
-  })
-  assert(response.title, 'response.title should be non-empty')
-  assert(response.body, 'response.body should be non-empty')
+  const taskResponse = await performTask(task, context)
+  if (taskResponse === null) {
+    return
+  }
 
   const gitStatus = await git.status()
   if (gitStatus === '') {
     return
   }
-
   const baseBranch = (await git.getDefaultBranch()) ?? 'main'
   const headBranch = `bot--tasks-${task.name.replaceAll(/[^\w]/g, '-')}`
   const workflowRunUrl = `${context.serverUrl}/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`
-  await git.commit(response.title, [workflowRunUrl])
+  await git.commit(taskResponse.title, [workflowRunUrl])
 
   const [owner, repo] = repository.split('/')
   assert(owner, 'repository must have an owner part')
   assert(repo, 'repository must have a repo part')
   const signedCommitSHA = await signCommit(owner, repo, octokit)
-  await git.push(`${signedCommitSHA}`, `refs/heads/${headBranch}`)
+  await git.push(signedCommitSHA, `refs/heads/${headBranch}`)
 
   const pull = await createOrUpdatePullRequest(octokit, {
     owner,
     repo,
-    title: response.title,
+    title: taskResponse.title,
     head: headBranch,
     base: baseBranch,
-    body: response.body,
+    body: taskResponse.body,
   })
   await octokit.rest.pulls.requestReviewers({
     owner,
