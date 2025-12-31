fe3141d9898ff8d3dcaffa1c6a7435206755e734
Hidetake Iwata <int128@gmail.com>
2025-12-31 13:23:29 +0900

    Add wait-for-workflows workflow (#383)
    * Add wait-for-workflows workflow
    
    * Fix
    
    * Fix
    
    * Use jq
    
    * Skip the coding agent
    
    * Refactor
    
    * Next

---
+++ tasks/wait-for-workflows/README.md
@@ -0,0 +1,4 @@
+# Add wait-for-workflows workflow for automerge
+
+This adds wait-for-workflows workflow.
+It is required for enabling `platformAutomerge` in Renovate.
---
+++ tasks/wait-for-workflows/precondition.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eux -o pipefail
+
+if [[ ! -d .github/workflows ]]; then
+  exit 99 # Skip the task
+fi
+
+if [[ -f .github/workflows/wait-for-workflows.yaml ]]; then
+  exit 99 # Skip the task
+fi
+
+cp /home/runner/work/actions-tanpopo/actions-tanpopo/.github/workflows/wait-for-workflows.yaml .github/workflows/wait-for-workflows.yaml
+
+jq '.platformAutomerge = true' .github/renovate.json > .github/renovate.tmp.json
+mv .github/renovate.tmp.json .github/renovate.json
+
+exit 109 # Skip the coding agent
