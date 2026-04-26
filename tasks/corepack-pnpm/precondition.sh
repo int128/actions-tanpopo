db7150ca26b73a488fb620e0811a785b204c25b8
Hidetake Iwata <int128@gmail.com>
2026-04-26 16:58:50 +0900

    Fix

--- tasks/corepack-pnpm/precondition.sh
+++ tasks/corepack-pnpm/precondition.sh
@@ -10,7 +10,7 @@ if ! grep 'npm install -g corepack' .github/workflows/*; then
 fi
 
 # Replace pnpm installation with corepack enable
-perl -i -pe 's/( *)- run: npm install -g pnpm@latest-10$/\1- run: npm install -g corepack\n\1- run: corepack enable/g' .github/workflows/*
+perl -i -pe 's/( *)- run: npm install -g pnpm@.+/\1- run: npm install -g corepack\n\1- run: corepack enable/g' .github/workflows/*
 
 # Remove "package-manager-cache: false" if it exists
 perl -i -pe 's/ +package-manager-cache: false\n//g' .github/workflows/*
