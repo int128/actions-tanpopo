659589c15fe58e000cbcc1b9f7d4d22573369a67
Hidetake Iwata <int128@gmail.com>
2026-09-21 15:19:57 +0900

    Migrate to @octokit/openapi-webhooks-types

--- src/github.ts
+++ src/github.ts
@@ -1,8 +1,8 @@
 import assert from 'node:assert'
 import * as fs from 'node:fs/promises'
 import { Octokit } from '@octokit/action'
+import type { webhooks } from '@octokit/openapi-webhooks-types'
 import { retry } from '@octokit/plugin-retry'
-import type { WebhookEvent } from '@octokit/webhooks-types'
 
 export const getOctokit = () => new (Octokit.plugin(retry))()
 
@@ -10,6 +10,9 @@ export type Repository = {
   owner: string
   repo: string
 }
+
+type Payload<Event extends keyof webhooks> = webhooks[Event]['post']['requestBody']['content']['application/json']
+type PossiblePayload = Payload<keyof webhooks>
 
 export type Context = {
   repo: Repository
@@ -19,7 +22,7 @@ export type Context = {
   runnerTemp: string
   serverUrl: string
   workspace: string
-  payload: WebhookEvent
+  payload: PossiblePayload
 }
 
 export const getContext = async (): Promise<Context> => {
@@ -32,7 +35,7 @@ export const getContext = async (): Promise<Context> => {
     runnerTemp: getEnv('RUNNER_TEMP'),
     serverUrl: getEnv('GITHUB_SERVER_URL'),
     workspace: getEnv('GITHUB_WORKSPACE'),
-    payload: JSON.parse(await fs.readFile(getEnv('GITHUB_EVENT_PATH'), 'utf-8')) as WebhookEvent,
+    payload: JSON.parse(await fs.readFile(getEnv('GITHUB_EVENT_PATH'), 'utf-8')) as PossiblePayload,
   }
 }
 
